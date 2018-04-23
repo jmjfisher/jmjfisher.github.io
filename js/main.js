384a89f7c68e80b2126b64976f84c7bbd3db549b
JohnMark Fisher <fisherjohnmark@gmail.com>
2018-04-23 15:24:02 -0400

    added chicago and olympics

--- js/main.js
+++ js/main.js
@@ -26,29 +26,39 @@ $(document).ready(function(){
 function displayCaption(){
     var id = this.id;
     if (id == "chile-thumb"){
-        $("#caption-1")
+        $("#caption-2")
             .hide()
             .html("<p class='caption'>Shaded relief map of a proposed cycling race route in Chile. Created using ArcMap and Adobe Photoshop and Illustrator.</p>")
             .slideDown(250);
     } else if (id =="transfer-thumb") {
-        $("#caption-1")
+        $("#caption-2")
             .hide()
             .html("<p class='caption'>Dynamic web application mapping annual football transfer fees by the world's most valuable clubs. Written in JavaScript with use of Leaflet and jQuery libraries.</p>")
             .slideDown(250);
     } else if (id == "ed-thumb"){
-        $("#caption-3")
+        $("#caption-4")
             .hide()
             .html("<p class='caption'>Longform infographic regarding education spending in the United States. Created using Adobe Illustrator, Excel, and indiemapper.</p>")
             .slideDown(250);
     } else if (id == "cycle-thumb"){
-        $("#caption-2")
+        $("#caption-3")
             .hide()
             .html("<p class='caption'>Interactive web application allowing users to obtain safe yet efficient cycling routes within Duluth, MN. Developed using Java, JavaScript, PostgreSQL / PostGIS, GoogleMaps API, and pgRouting. (video demo)</p>")
             .slideDown(250);
     } else if (id == "prince-thumb"){
-        $("#caption-2")
+        $("#caption-3")
             .hide()
             .html("<p class='caption'>Mapbox Studio tileset inspired by Antoine de Saint-Exup&eacute;ry's <i>The Little Prince</i>.</p>")
+            .slideDown(250);
+    } else if (id == "chicago-thumb"){
+        $("#caption-1")
+            .hide()
+            .html("<p class='caption'>A research application to convey areas of potential population displacement or gentrification in Chicago. Written in JavaScript with use of Leaflet and D3 libraries.</p>")
+            .slideDown(250);
+    } else if (id == "olympics-thumb"){
+        $("#caption-1")
+            .hide()
+            .html("<p class='caption'>An application to visualize medal performance at various Olympic games with respect to GDP. Written in JavaScript with use of D3 library.</p>")
             .slideDown(250);
     } else {
         clearCaption
