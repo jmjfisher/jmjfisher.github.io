cab76e31fa02574958ec3be1dc7ff67e21bfda43
JohnMark Fisher <fisherjohnmark@gmail.com>
2019-06-06 12:53:27 -0400

    spring cleaning (updates)

--- js/main.js
+++ js/main.js
@@ -28,12 +28,12 @@ function displayCaption(){
     if (id == "chile-thumb"){
         $("#caption-2")
             .hide()
-            .html("<p class='caption'>Shaded relief map of a proposed cycling race route in Chile. Created using ArcMap and Adobe Photoshop and Illustrator.</p>")
+            .html("<p class='caption'>Shaded relief map of a proposed cycling race route in Chile. ArcMap and Adobe Photoshop and Illustrator.</p>")
             .slideDown(250);
     } else if (id == "radio-thumb") {
         $("#caption-3")
             .hide()
-            .html("<p class='caption'>Mobile-friendly interactive map helping users find nearest Packers Radio Network affiliate station based on Voronoi polygons calculation. Created using QGIS data analysis and editing tools and Leaflet.</p>")
+            .html("<p class='caption'>Mobile-friendly interactive map helping users find nearest Packers Radio Network affiliate station based on Voronoi polygons calculation. QGIS data analysis and editing tools and Leaflet.</p>")
             .slideDown(250);
     } else if (id =="lp-thumb") {
         $("#caption-5")
@@ -48,22 +48,22 @@ function displayCaption(){
     } else if (id =="bus-thumb") {
         $("#caption-2")
             .hide()
-            .html("<p class='caption'>Scrollytelling visual essay about decreasing bus ridership in DC. Written in JavaScript with use of Leaflet, D3, Scrollama, and jQuery libraries, Adobe Illustrator graphics, and ArcMap analysis.</p>")
+            .html("<p class='caption'><i>Scrollytelling</i> visual essay about decreasing bus ridership in DC. Leaflet, D3, Scrollama, and jQuery libraries, Adobe Illustrator graphics, and ArcMap analysis.</p>")
             .slideDown(250);
     } else if (id =="transfer-thumb") {
         $("#caption-4")
             .hide()
-            .html("<p class='caption'>Dynamic web application mapping annual football transfer fees by the world's most valuable clubs. Written in JavaScript with use of Leaflet and jQuery libraries.</p>")
+            .html("<p class='caption'>Web application mapping annual football transfer fees by the world's most valuable clubs. Leaflet and jQuery.</p>")
             .slideDown(250);
     } else if (id == "ed-thumb"){
-        $("#caption-4")
+        $("#caption-6")
             .hide()
-            .html("<p class='caption'>Longform infographic regarding education spending in the United States. Created using Adobe Illustrator, Excel, and indiemapper.</p>")
+            .html("<p class='caption'>Longform infographic regarding education spending in the United States. Adobe Illustrator, Excel, and indiemapper.</p>")
             .slideDown(250);
     } else if (id == "cycle-thumb"){
         $("#caption-6")
             .hide()
-            .html("<p class='caption'>Interactive web application allowing users to obtain safe yet efficient cycling routes within Duluth, MN. Developed using Java, JavaScript, PostgreSQL / PostGIS, GoogleMaps API, and pgRouting. (video demo)</p>")
+            .html("<p class='caption'>Web application allowing users to obtain safe yet efficient cycling routes within Duluth, MN. Java, JavaScript, PostgreSQL (PostGIS), GoogleMaps API, and pgRouting. (video demo)</p>")
             .slideDown(250);
     } else if (id == "prince-thumb"){
         $("#caption-3")
@@ -73,12 +73,17 @@ function displayCaption(){
     } else if (id == "chicago-thumb"){
         $("#caption-1")
             .hide()
-            .html("<p class='caption'>A research application to convey areas of potential population displacement or gentrification in Chicago. Written in JavaScript with use of Leaflet and D3 libraries.</p>")
+            .html("<p class='caption'>A research application to convey areas of potential population displacement or gentrification in Chicago. Leaflet and D3 libraries.</p>")
             .slideDown(250);
     } else if (id == "olympics-thumb"){
         $("#caption-1")
             .hide()
-            .html("<p class='caption'>An application to visualize medal performance at various Olympic games with respect to GDP. Written in JavaScript with use of D3 library.</p>")
+            .html("<p class='caption'>An application to visualize national medal performances at recent Olympic games with respect to GDP. JavaScript and D3.</p>")
+            .slideDown(250);
+    } else if (id == "uw-thumb"){
+        $("#caption-4")
+            .hide()
+            .html("<p class='caption'>Leafet map with Carto (PostGIS) backing showing remote <i>Uni Watch</i> party locations.</p>")
             .slideDown(250);
     } else {
         clearCaption
