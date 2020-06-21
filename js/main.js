04f9d4f9d8b5e543f32b60b9097e653f58ada5d6
JohnMark Fisher <fisherjohnmark@gmail.com>
2020-06-21 07:59:03 -0500

    + Be The Ball

--- js/main.js
+++ js/main.js
@@ -64,12 +64,12 @@ function displayCaption(){
                 .html("<p class='caption'>Mobile-friendly interactive map helping users find nearest Packers Radio Network affiliate station based on Voronoi polygons calculation. QGIS data analysis and editing tools and Leaflet.</p>")
                 .slideDown(250);
         } else if (id =="lp-thumb") {
-            $("#caption-5")
+            $("#caption-6")
                 .hide()
                 .html("<p class='caption'>Interactive map showing POIs in Manitowoc Lincoln Park. Makes jQuery POST and GET calls to PostGIS database to update map with comments or show nearest POI to a point. (video demo)</p>")
                 .slideDown(250);
         } else if (id =="arcpy-thumb") {
-            $("#caption-5")
+            $("#caption-6")
                 .hide()
                 .html("<p class='caption'>ArcPy (Tkinter GUI) application running IDW and GWR analyses on relationship between nitrate rates in wells and cancer rates in Wisconsin. (video demo)</p>")
                 .slideDown(250);
@@ -79,7 +79,7 @@ function displayCaption(){
                 .html("<p class='caption'>An analysis of the nationalities of Premier League players over time. Data gathered and organized with python (Beautiful Soup + pandas) with Tableau Public and Leaflet visualizations.</p>")
                 .slideDown(250);
         } else if (id =="bus-thumb") {
-            $("#caption-2")
+            $("#caption-5")
                 .hide()
                 .html("<p class='caption'><i>Scrollytelling</i> visual essay about decreasing bus ridership in DC. Leaflet, D3, Scrollama, and jQuery libraries, Adobe Illustrator graphics, and ArcMap analysis.</p>")
                 .slideDown(250);
@@ -89,7 +89,7 @@ function displayCaption(){
                 .html("<p class='caption'>Web application mapping annual football transfer fees by the world's most valuable clubs. Leaflet and jQuery.</p>")
                 .slideDown(250);
         } else if (id == "ed-thumb"){
-            $("#caption-6")
+            $("#caption-7")
                 .hide()
                 .html("<p class='caption'>Longform infographic regarding education spending in the United States. Adobe Illustrator, Excel, and indiemapper.</p>")
                 .slideDown(250);
@@ -104,12 +104,12 @@ function displayCaption(){
                 .html("<p class='caption'>Mapbox Studio tileset inspired by Antoine de Saint-Exup&eacute;ry's <i>The Little Prince</i>.</p>")
                 .slideDown(250);
         } else if (id == "chicago-thumb"){
-            $("#caption-1")
+            $("#caption-2")
                 .hide()
                 .html("<p class='caption'>A research application to convey areas of potential population displacement or gentrification in Chicago. Leaflet and D3 libraries.</p>")
                 .slideDown(250);
         } else if (id == "olympics-thumb"){
-            $("#caption-6")
+            $("#caption-5")
                 .hide()
                 .html("<p class='caption'>An application to visualize national medal performances at recent Olympic games with respect to GDP. JavaScript and D3.</p>")
                 .slideDown(250);
@@ -118,12 +118,15 @@ function displayCaption(){
                 .hide()
                 .html("<p class='caption'>Leafet map with Carto (PostGIS) backing showing remote <i>Uni Watch</i> party locations.</p>")
                 .slideDown(250);
+        } else if (id == "btb-thumb"){
+            $("#caption-1")
+                .hide()
+                .html("<p class='caption'>Data-lite golf yardage web-app measuring user's HTML Geolocation API location against PostGIS points with a <i>Caddyshack</i> theme.</p>")
+                .slideDown(250);
         } else {
             clearCaption
-        }
-    }
-        
-
+        };
+    };
 };
 
 function clearCaption(){
